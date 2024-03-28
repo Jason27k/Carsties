1a191bfba3922a2953adf78124c13878704767c9
Jason27k <68726620+Jason27k@users.noreply.github.com>
2024-03-27 21:51:47 -0700

    End of section 2

---
+++ src/AuctionService/Data/Migrations/20240328023328_Reserve Fix.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReserveFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResrvePrice",
+                table: "Auctions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ResrvePrice",
+                table: "Auctions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
